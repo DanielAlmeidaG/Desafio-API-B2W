8a3bf4335e975bee6920fcbcf78308ec5fae83d8
DanielAlmeidaG <daniel_garrido@id.uff.br>
2020-05-17 00:26:03 -0300

    Commit de organização

--- src/main/java/com/desafio/b2w/apirest/controller/PlanetController.java
+++ src/main/java/com/desafio/b2w/apirest/controller/PlanetController.java
@@ -24,7 +24,7 @@ import com.desafio.b2w.apirest.repository.PlanetRepository;
 @RequestMapping("/planets")
 public class PlanetController {
 	
-	@Autowired 
+	@Autowired  
 	PlanetRepository repository;
 
 	@GetMapping("/")
